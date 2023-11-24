59510a587ddc00d7d66086c22c386873ed2df878
TomasFerreccio <tferreccio@itba.edu.ar>
2023-11-24 18:47:50 -0300

    Ultimos Detalles
    AA

--- merch.js
+++ merch.js
@@ -16,7 +16,7 @@ const merch = [
     
     {
         id: 3,
-        nombre: "Camiseta Jugador",
+        nombre: "Camiseta Titular",
         precio: 35000,
         img: "img/player.avif",
     },
@@ -24,36 +24,64 @@ const merch = [
     {
         id: 4,
         nombre: "Camiseta Leo Messi",
-        precio: 35000,
+        precio: 45000,
         img: "img/messi1.00.webp",
     },
 
-    // {
-    //     id: 5,
-    //     nombre: "Camiseta Leo Messi",
-    //     precio: 35000,
-    //     img: "img/messi1.00.webp",
-    // },
+    {
+        id: 5,
+        nombre: "Camiseta Alternativa",
+        precio: 35000,
+        img: "img/suplente.avif",
+    },
 
-    // {
-    //     id: 6,
-    //     nombre: "Camiseta Leo Messi",
-    //     precio: 35000,
-    //     img: "img/messi1.00.webp",
-    // },
+    {
+        id: 6,
+        nombre: "Short Titular",
+        precio: 15000,
+        img: "img/short.avif",
+    },
 
-    // {
-    //     id: 7,
-    //     nombre: "Camiseta Leo Messi",
-    //     precio: 35000,
-    //     img: "img/messi1.00.webp",
-    // },
+    {
+        id: 7,
+        nombre: "Chomba Argentina",
+        precio: 28000,
+        img: "img/chomba.avif",
+    },
 
-    // {
-    //     id: 8,
-    //     nombre: "Camiseta Leo Messi",
-    //     precio: 35000,
-    //     img: "img/messi1.00.webp",
-    // },
+    {
+        id: 8,
+        nombre: "Campera Argentina",
+        precio: 60000,
+        img: "img/campera.avif",
+    },
+
+    {
+        id: 9,
+        nombre: "Musculosa Arg",
+        precio: 22500,
+        img: "img/muscu.avif",
+    },
+
+    {
+        id: 10,
+        nombre: "Campera Violeta",
+        precio: 85000,
+        img: "img/camperita.avif",
+    },
+
+    {
+        id: 11,
+        nombre: "Pantalon Deportivo",
+        precio: 32000,
+        img: "img/pantalon1.avif",
+    },
+
+    {
+        id: 12,
+        nombre: "Superstars Arg",
+        precio: 90000,
+        img: "img/superstar.avif",
+    },
 
 ];
